c57a93d172e9fd79b204975b679ec0482af3674c
AndriiBulhakov <andrijbulgakov5@gmail.com>
2024-03-28 10:52:12 +0100

    add server routes

--- server/routes/works.js
+++ server/routes/works.js
@@ -2,20 +2,20 @@
 import express from "express";
 const router = express.Router();
 
+// example of a route that fetches data from the Webflow API
 router.get("/", async (req, res) => {
     try {
-        const url = "https://api.webflow.com/v2/collections/65eec59c0fd2d28579f05f0b/items";
+        const url = "https://api.webflow.com/v2/collections/id/items";
         const options = {
             method: "GET",
             headers: {
                 accept: "application/json",
-                authorization: "Bearer cbbb60a29e31382e01dd86addb054c6b4b5fa28f7d209e7cc8778046751e656e",
+                authorization: "auth",
             },
         };
 
         const response = await fetch(url, options);
         const json = await response.json();
-        console.log(json);
         res.send(json.items);
     } catch (err) {
         console.error("Error:", err);
